048c24d859de12be03b78bcf6fc5900394329e56
Annie Truex <artruex@gmail.com>
2019-05-18 03:06:00 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,15 +1,9 @@
 def my_each(words) do |word|
-   i 
-   counter = 0 
+   i = 0 
   puts word
-  while 
-  counter+=1
+  while i <array.length 
+  yield(array[i])
+  i +=1
 end
-
-  i = 0
-  while i < array.length
-    yield(array[i])
-    i += 1
-  end
-  array
+array
 end
